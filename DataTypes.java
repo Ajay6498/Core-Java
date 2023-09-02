501367115c0bb0677fad2abac4651a970637e315
ajay <ajaybawakar19@gmail.com>
2023-09-02 23:16:29 +0530

    first Commit

---
+++ DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes{
+     public static void main(String[] args) {
+       String s="Ajay";
+       int i=10;
+       Double d=20.00;
+       char c='A';
+       System.out.println("String data"+s); 
+       System.out.println("Integer"+i);
+       System.out.println("Double"+d);
+       System.out.println("Charactor"+c);
+     }
+    
+     
+}
