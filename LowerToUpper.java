724b09a32264ae153b58adc20e08f0b6a35c81e0
ajay <ajaybawakar19@gmail.com>
2023-09-03 09:17:28 +0530

    fc

---
+++ LowerToUpper.java
@@ -0,0 +1,7 @@
+public class LowerToUpper {
+    public static void main(String[] args) {
+        String str="Hello";
+        String str2=str.toUpperCase();
+        System.out.println(str2);
+    }
+}
